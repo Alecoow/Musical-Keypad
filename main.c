ad02333d712a84d7f6f1242eba070c8a94ea9fd9
Alecoow <alecoow@gmail.com>
2025-03-21 14:25:53 -0700

    Update main.c

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
  * Created: 3/13/2025 11:30:37 PM
  * Author : Alex Cooper
  */ 
-
+#define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 #include <math.h>
-#define F_CPU 16000000UL
 
-const unsigned char keypad[4][4] = {
+
+const unsigned int keypad[4][4] = {
 	{0, 1, 2, 3},
 	{4, 5, 6, 7},
 	{8, 9, 10, 11},
@@ -27,11 +27,12 @@ void Init() {
 	DDRC |= (1 << 4) | (1 << 5); // audio output
 }
 
-char Keypad_Scan() {
+int Keypad_Scan() {
 	for (int i = 4; i < 8; i++)  {
 		PORTD &= ~(1 << i); // Disable row pullup resistor
 		for (int j = 0; j < 4; j++) {
 			if (!(PINB & (1 << j))) {
+				PORTD |= (1 << i); // reenable row pullup resistor
 				return keypad[i-4][j];
 			}
 		}
@@ -40,11 +41,11 @@ char Keypad_Scan() {
 	return '?';
 }
 
-void Timer_Count(float frequency) {
-	TCCR0A |= (1 << WGM01);
-	OCR0A = frequency;
+void Play_Sound(float frequency) {
+	TCCR0A |= (1 << WGM01); // ctc
+	OCR0A = frequency; // 
 	OCR0B = frequency / 2.0; // 50% duty cycle
-	TCCR0B |= (1 << 2) | (1 << 0);
+	TCCR0B |= (1 << CS02); // prescale 256
 	PORTC |= (1 << 4) | (1 << 5); // enable speaker output
 	
 	while (!(TIFR0 & (1 << OCF0B))) {} // wait for OCR0B overflow
@@ -56,23 +57,31 @@ void Timer_Count(float frequency) {
 	TIFR0 |= 1 << OCF0A; // reset flag
 }
 
-float Calculate_Frequency(char key) { 
-	double period = 1.059463; // 2 ^ (1/12)
-	period = pow(period, (double)key); // period ^ key
-	return (1.0 / (440 * period)) * 16000.0; // 1 / (440HZ * period) * clock speed
+float Calculate_Frequency(int key) { 
+	float period = 1.059463; // 2 ^ (1/12)
+	period = 440 *(pow(period, (double)key)); // 440 (middle frequency) * period ^ key
+	return (F_CPU/(256*period)) - 1;
 }
-
+/*
+	float period = 1.059463; // 2 ^ (1/12)
+	period = 440 *(pow(period, (double)key)); // 440 * period ^ key
+	return (F_CPU/(256*period)) - 1;
+	
+	float frequency = 440 * pow(pow(2.0, (1.0 / 12.0)), index);
+	return (F_CPU/(256*frequency)) - 1;
+*/
 int main(void) {
 	Init(); // Set up register flags
 	
     while (1) {
-		char input = Keypad_Scan(); // Scan keypad for input
+		int input = Keypad_Scan(); // Scan keypad for input
 		if (input != '?') {
 			float key = Calculate_Frequency(input);
-			Timer_Count(key);
+			Play_Sound(key);
 		}
     }
 	
 	return 0;
 }
+
 
